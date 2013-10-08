4c559fe9d51e7d3c931d8397f461bd8613779bff
Сергеенко С.В <sserg.me@gmail.com>
2013-10-08 15:47:29 +0300

    Допускаем многословные имена.
    Считываем в качестве имени всю введенную пользователем строку, а не только
    первое слово

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
 	std::cout << "Enter your name:";
 	std::string name;
-	std::cin >> name;
+	std::getline(std::cin, name);
 	std::cout << "Hello, " << name << "!" << std::endl;
 	return 0;
 }
